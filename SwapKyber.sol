ba9d45b720da75474885cede244347e614a9cdd4
Robert Miller <bert.c.miller@gmail.com>
2020-09-29 20:28:10 -0400

    Update SwapKyber.sol

--- SwapKyber.sol
+++ SwapKyber.sol
@@ -17,13 +17,30 @@ contract SwapKyber {
     dai_token = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     yfi_token = IERC20(0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e);
 
-    kyberProxy = IKyberNetworkProxy(0x818E6FECD516Ecc3849DAf6845e3EC868087B755);
+    kyberProxy = IKyberNetworkProxy(0x9AAb3f75489902f3a48495025729a0AF77d4b11e);
   }
 
-  function callfunc() public view returns (uint256) {
-    /* uint256 expectedRate = kyberProxy.getExpectedRateAfterFee(yfi_token, dai_token, 10000, 25, ''); */
-    uint256 expectedRate = kyberProxy.getExpectedRateAfterFee(0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e, 0x6B175474E89094C44Da98b954EedeAC495271d0F, 10000, 25, '');
-    /* (uint256 expectedRate, ) = kyberProxy.getExpectedRate(yfi_token, dai_token, 1000000); */
+  function getexpectedrate(uint ethAmount) public view returns (uint256) {
+    uint256 expectedRate = kyberProxy.getExpectedRateAfterFee(eth_token, dai_token, ethAmount, 25, '');
+
     return expectedRate;
   }
+
+  function tradetest(uint ethAmount) public payable returns (uint256) {
+    uint256 expectedRate = getexpectedrate(ethAmount);
+
+    uint256 actualDestAmount = kyberProxy.tradeWithHintAndFee{value: msg.value}(
+      eth_token, // ETH address
+      ethAmount, // 1 ETH -- SRCAMOUNT
+      dai_token, // KNC address
+      msg.sender, // destAddress
+      9999999999999999999999999999999, // maxDestAmount: arbitarily large to swap full amount
+      expectedRate, // minConversionRate: value from getExpectedRate call
+      msg.sender, // platform wallet
+      25, // 0.25%
+      '' // empty hint
+      );
+    return actualDestAmount;
+  }
+
 }
